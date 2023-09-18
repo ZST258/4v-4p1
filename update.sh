81388e4292c387d249916a72782974191d6147dc
root <root@VM-W4ZX189Q06VF.local>
2023-09-18 20:25:52 +0800

    Daily update 2023-09-18

--- update.sh
+++ update.sh
@@ -12,10 +12,10 @@ cd /api-project
 # 强制上传项目到GitHub仓库
 git add .
 git commit -m "Daily update $(date +\%Y-\%m-\%d)"
-git push "https://${github_username}:${github_token}@github.com/${github_username}/${repo_name}.git" "$branch_name"
+git push "https://${github_username}:${github_token}@github.com/${github_username}" "$branch_name"
 
 # 拉取最新版本
-git pull "https://${github_username}:${github_token}@github.com/${github_username}/${repo_name}.git" "$branch_name"
+git pull "https://${github_username}:${github_token}@github.com/${github_username}" "$branch_name"
 
 echo "项目已更新并上传到GitHub仓库，最新版本已拉取"
 
